14dd0942b385b67d504b1222ebf1e0c411a6e12f
Keeean <ken.suken04@gmail.com>
2025-07-03 18:25:56 +1000

    initial commit: working file relocation script

---
+++ fileRelocator.py
@@ -0,0 +1,44 @@
+# going to file relocation script.
+import os
+import argparse
+import shutil
+import logging
+
+logging.basicConfig()
+
+IMG_EXTENSIONS = ['.jpg', '.png', '.svg', '.jpeg']
+DOC_EXTENSIONS = ['.docx', '.pdf', '.DOCX', '.PDF', '.doc', '.ppt', '.pptx']
+
+def hasExtension(filename, extension):
+    return any(filename.lower().endswith(ext) for ext in extension)
+
+def findFiles(directory, extension):
+    return [f for f in os.listdir(directory) if hasExtension(f, extension)]
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', default = 'images', choices=['images', 'documents'])
+    parser.add_argument('--search_dir', default = "C:/Users/kensu/Downloads", help="where this script will be searching for files.")
+    parser.add_argument('--save_dir', default='C:/Users/kensu/OneDrive/Pictures', help="where files get sent")
+    parser.add_argument('--preview', action='store_true', help='showing where the files will be relocated.')
+    
+    args = parser.parse_args()
+
+    extensions = IMG_EXTENSIONS if args.mode == 'images' else DOC_EXTENSIONS
+
+    destination = args.save_dir
+    source = args.search_dir
+
+    files = findFiles(source, extensions)
+    
+    if args.preview: 
+        for file in files:
+            print(os.path.join(source, file) + " will be moved to " + os.path.join(destination, file))
+        print("File relocation preview finished.")
+    else:
+        for file in files:
+            shutil.move(os.path.join(source, file), os.path.join(destination, file))
+        print("\nFile relocation done. Please check the source and destination folders.")
+    
+    
